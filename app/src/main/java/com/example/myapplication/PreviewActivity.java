a830fa4de819b9d24b670c8235f2575147a6e3a3
TimurShubin <roomykzn2014@yandex.ru>
2023-05-30 15:28:11 +0300

    добавил экран регистрации

---
+++ app/src/main/java/com/example/myapplication/PreviewActivity.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PreviewActivity extends AppCompatActivity {
+
+    private Button btnSignInAsCoach;
+    private Button btnSignInAsSportsman;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.preview_activity);
+        this.getSupportActionBar().hide();
+
+        btnSignInAsCoach = findViewById(R.id.btnSignInAsCoach);
+        btnSignInAsSportsman = findViewById(R.id.btnSignInAsSportsman);
+
+        // пока что без авторизации
+        // экран авторизации в работе
+        btnSignInAsCoach.setOnClickListener(v -> {
+            // работа с тренером
+            Intent intent = new Intent(PreviewActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+        btnSignInAsSportsman.setOnClickListener(v -> {
+            // работа со спортсменом
+            Intent intent = new Intent(PreviewActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+    }
+}
